5b8b94648e0b58bca5b4fab5cb9ac0555d0718e0
Julien Pontbriand <nullzeal@pm.me>
2022-06-14 15:27:10 -0400

    finished ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...otherArgs) {
+    let newArray = array;
+    let argsArray = otherArgs;
 
-};
+    for (j=0; j < argsArray.length;j++) 
+    {
+        for(i=0;i<newArray.length;i++)
+        {
+                
+            if(newArray[i] === argsArray[j])
+            {
+                    newArray.splice(i,1);
+            }
+        }
+    }
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
