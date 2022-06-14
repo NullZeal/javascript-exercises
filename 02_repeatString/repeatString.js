bacc961aadfdbd0961b47f1fd90c63e7a66a664f
Julien Pontbriand <nullzeal@pm.me>
2022-06-14 12:49:04 -0400

    Finished exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, xTimesRepeated) {
+    if (xTimesRepeated < 0){
+        return 'ERROR';
+    }
+    newString = '';
+    for(i=0; i<xTimesRepeated; i++){
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
