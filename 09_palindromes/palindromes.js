86c3c5da43d50c4166528df0a74db89a0ed8f08f
nullzeal <nullzeal@pm.me>
2022-07-08 17:06:12 -0400

    first 4 tests of ex9 working

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    function isLetter(str) {return str.match(/[a-z]/i);}
+    let wordArray = word.split('');
+    let filteredWordArray = wordArray.filter(isLetter);
+    
+    let i = Math.floor(wordArray.length / 2);
+    console.log(filteredWordArray);
+
+    while(i > 0){
+        if(filteredWordArray.length == 1){return true;}
+        let firstLetter = filteredWordArray[0].toLowerCase();
+        let lastLetter = filteredWordArray[filteredWordArray.length - 1].toLowerCase();
+
+        if(firstLetter != lastLetter){return false;}
+        filteredWordArray.pop();
+        filteredWordArray.shift();
+
+        console.log(filteredWordArray);
+        i--;
+    }
+    return true;
 };
 
 // Do not edit below this line
