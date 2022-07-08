5af17e022d260ead913af636314275f28be697c6
nullzeal <nullzeal@pm.me>
2022-07-08 13:41:01 -0400

    Finished ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {return a + b;};
+
+const subtract = function(a, b) {return a - b;};
+
+const sum = function (arr) {return arr.reduce((total, num) => total + num, 0)};
+
+const multiply = function(arr) {return arr.reduce((a , b) => a * b)};
+
+const power = function(num, power) {
+  if (power == 0){return 1;}
+  else if(power == 1){return num;}
+
+  let arr = [];
+  for (let i = 0; i<power;i++){arr.push(num)};
+  return arr.reduce((a, b) => a * b );
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  if(num == 0){return 1}
+  
+  let arr = [];
+  let i = num;
+  
+  while (i > 0){
+      arr.push(i);
+      i--;
+    }
+    return arr.reduce((a, b) => a * b);
+}
 
 // Do not edit below this line
 module.exports = {
